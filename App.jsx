b0f2afa488f47a86ddbfec8c3be659f332e45f1c
Prince016 <prashantsharma3854@gmail.com>
2025-02-25 00:00:38 +0530

    second

--- App.jsx
+++ App.jsx
@@ -1,15 +1,27 @@
 
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const App = () => {
+
+
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+
+    <SafeAreaView style={styles.container}>
+      <Text>rocky bhi</Text>
+      <Pressable>
+        <Text>Press me </Text>
+      </Pressable>
+    </SafeAreaView>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    width:"100%",
+    height:"100%",
+    backgroundColor:"yellow",
+  }
+})
