ebaf29d5ac2979e2e0df49356b1811a5be3fc644
Prashantsha8586 <prashantsha8586@gmail.com>
2025-03-01 10:40:51 +0530

    till styling

--- App.jsx
+++ App.jsx
@@ -1,27 +1,43 @@
-
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import React from 'react';
 
 const App = () => {
-
+  const theme = useColorScheme();
+  const isDarkMode = theme === 'dark';
 
   return (
+    // <SafeAreaView style={[styles.container, {
+    //   backgroundColor: isDarkMode ? "black" : "white"
+    // }]}>
 
     <SafeAreaView style={styles.container}>
-      <Text>rocky bhi</Text>
+      <Text style={styles.text}>rocky bhai</Text>
       <Pressable>
         <Text>Press me </Text>
       </Pressable>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
-  container:{
-    width:"100%",
-    height:"100%",
-    backgroundColor:"yellow",
-  }
-})
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'yellow',
+  },
+  button: {
+    paddingHorizontal: 10,
+  },
+
+  text: {
+    color: isDarkMode ? 'white' : 'black',
+  },
+});
